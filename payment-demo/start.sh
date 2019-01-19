174c4fe43f711f0c01e6acdc9e0f69a1e2127365
wangxiang <wangxiang19870507@gmail.com>
2019-01-19 15:33:56 +0800

    export CLIENT_AMOUNT=80

--- payment-demo/start.sh
+++ payment-demo/start.sh
@@ -1,7 +1,7 @@
 #dep init
 go build
 # one client in one goroutine. If client amoutn is less than accounts, the tps of CreateAccount can be low.
-export CLIENT_AMOUNT=1000
+export CLIENT_AMOUNT=80
 export ACCOUNTS=1000
 export AMOUNT=60
 ./payment-demo
